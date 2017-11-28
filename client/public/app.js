793e2315feecbc969e820f72a95db1dcad342f88
Ciaran <ciarantsc@gmail.com>
2017-11-28 08:33:38 +0000

    brainstorming initial code in app.js

--- client/public/app.js
+++ client/public/app.js
@@ -1,5 +1,31 @@
 var App = function () {
-  this.volume = 0;
+	var grid;
+	var cols = 20;
+	var rows = 20;
 }
+
+App.prototype = {
+
+	make2DArray(cols, rows) {
+		var arr = new Array(cols);
+		for (var i = 0; i < arr.length; i++) {
+			arr[i] = new Array(rows);
+		}
+	}
+
+	Cell() {
+		this.mine = true;
+		this.revealed = true;
+	}
+
+	setup() {
+		createCanvas(200, 200);
+		grid = make2DArray(20, 20);
+	}
+
+
+}
+
+console.log("This is app.js");
 
 module.exports = App;
