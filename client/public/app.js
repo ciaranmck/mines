b7a2fb2ffd3bfc2d789fc4f4217eecb3e5e0bf69
Ciaran <ciarantsc@gmail.com>
2017-12-01 06:18:55 +0000

    functions added to app class, cell class exported

--- client/public/app.js
+++ client/public/app.js
@@ -1,31 +1,30 @@
+var Cell = require("./cell.js");
+
 var App = function () {
-	var grid;
-	var cols = 20;
-	var rows = 20;
+
+};
+
+App.prototype.make2DArray = function(cols, rows) {
+	var arr = new Array(cols);
+	for (var i = 0; i < arr.length; i++) {
+		arr[i] = new Array(rows);
+	}
 }
 
-App.prototype = {
-
-	make2DArray(cols, rows) {
-		var arr = new Array(cols);
-		for (var i = 0; i < arr.length; i++) {
-			arr[i] = new Array(rows);
+var grid;
+var cols = 20;
+var rows = 20;
+ 
+function setup() {
+	createCanvas(200, 200);
+	grid = make2DArray(cols, rows);
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			grid[i][j] = new Cell;
 		}
 	}
 
-	Cell() {
-		this.mine = true;
-		this.revealed = true;
-	}
-
-	setup() {
-		createCanvas(200, 200);
-		grid = make2DArray(20, 20);
-	}
-
-
 }
 
-console.log("This is app.js");
 
 module.exports = App;
---
+++ client/public/cell.js
@@ -0,0 +1,6 @@
+function Cell() {
+	this.mine = true;
+	this.revealed = true;
+}
+
+module.exports = Cell;
