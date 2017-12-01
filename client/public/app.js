4cd47162c76ccab76228c9cc7ba8517bf4ee170f
Ciaran <ciarantsc@gmail.com>
2017-12-01 06:29:06 +0000

    show function written in cell.js

--- client/public/app.js
+++ client/public/app.js
@@ -1,7 +1,10 @@
 var Cell = require("./cell.js");
 
 var App = function () {
-
+	var grid;
+	var cols = 20;
+	var rows = 20;
+	var w = 20;
 };
 
 App.prototype.make2DArray = function(cols, rows) {
@@ -9,21 +12,30 @@ App.prototype.make2DArray = function(cols, rows) {
 	for (var i = 0; i < arr.length; i++) {
 		arr[i] = new Array(rows);
 	}
+	return arr;
 }
 
-var grid;
-var cols = 20;
-var rows = 20;
+
  
-function setup() {
+App.prototype.setup = function() {
 	createCanvas(200, 200);
+	cols = floor(width / w);
+	rows = floor(height / w);
 	grid = make2DArray(cols, rows);
 	for (var i = 0; i < cols; i++) {
 		for (var j = 0; j < rows; j++) {
-			grid[i][j] = new Cell;
+			grid[i][j] = new Cell(i * w, j * w);
 		}
 	}
+}
 
+App.prototype.draw = function() {
+	background(0);
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			grid[i][j].show();
+		}
+	}
 }
 
 
--- client/public/cell.js
+++ client/public/cell.js
@@ -1,6 +1,10 @@
-function Cell() {
+function Cell(x, y, w) {
 	this.mine = true;
 	this.revealed = true;
+}
+
+Cell.prototype.show = function() {
+	rect(this.x, this.y, this.w);
 }
 
 module.exports = Cell;
