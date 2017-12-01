82a1f513c24e74d9401e9a5d1e2281e90451dc21
Ciaran <ciarantsc@gmail.com>
2017-12-01 07:18:37 +0000

    cells can be clicked on to reveal whether there is a mine or not

--- client/public/cell.js
+++ client/public/cell.js
@@ -20,3 +20,11 @@ Cell.prototype.show = function() {
 		}
 	}
 }
+
+Cell.prototype.contains = function(x, y) {
+	return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w);
+}
+
+Cell.prototype.reveal = function() {
+	this.revealed = true;
+}
